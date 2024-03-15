f92fc59d5da7b3a7ba113ad3c7d211df2696b781
Matthew Camarena <camarena.matthew@gmail.com>
2024-03-15 17:15:06 -0500

    Update index.js
    users

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+
 });
 
-//need connection to mongodb
-
+//mongodb connection
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
 
 //create schemas for users/ logs
 
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  count: Number,
+  log:[{
+    
+      description: [String],
+      duration: Number,
+      date: Date,
+    
+  }]
+})
+
+const User = mongoose.model("User", userSchema);
+
 app.post('/api/users', (req,res) =>{
+  var username = req.body.username;
+  var newUser = new User({username: username})
+  newUser.save();
+  var userId = newUser._id.toString();
+  res.json({username: username, "_id": userId});
   // body users create new user
   // return _id from username
-});
 
+
+});
 app.get('/api/users', (req,res) => {
   // return list of all users array so user + id as json i assume
   
